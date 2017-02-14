b6e95d7362a8335eefe8dab02a20895ec0d7d9f1
Sean Rose <sean1rose@gmail.com>
2017-02-13 23:49:29 -0800

    add comment

--- productOfOtherNumbers.js
+++ productOfOtherNumbers.js
@@ -1,6 +1,7 @@
-// var list = [1, 7, 3, 4]
+// var list = [1, 7, 3, 4];
 
-var getProductsOfAllIntsExceptAtIndex(arr) = () => {
+// O(n)^2 solution
+var getProductsOfAllIntsExceptAtIndex = (arr) => {
   // brute force
   var result = [];
   for (var i = 0; i < arr.length; i++){
@@ -13,4 +14,43 @@ var getProductsOfAllIntsExceptAtIndex(arr) = () => {
     }
     result.push(product);
   }
+  return result;
 }
+
+
+
+// 2ND ANSWER...
+[2, 4, 10]
+// the product of all integers except the integer at each index is equal to #1 & #2
+// BREAK DOWN INTO 2 COMPONENT SUB PARTS
+  // 1. find product of all integers before each index
+  // 2. find product of all integers after each index.
+
+var getProductsOfAllIntsExceptAtIndex = (intArray) => {
+  var productsOfAllIntsExceptAtIndexResult = [];
+
+  // 1. finding product of all integers BEFORE each index...
+  var productSoFar = 1;
+  // for each integer, find the [product of all the integers before it], storing the [total product so far] each time
+  for (var j = 0; j < intArray.length; j++){
+    productsOfAllIntsExceptAtIndexResult[j] = productSoFar;
+    productSoFar *= intArray[j];
+  }
+  // [1, 2, 8]
+
+
+  // 2. find product of all integers AFTER each index...
+  // HOW? -> walk through our array backwards! reverse-for-loop!
+  // var productsOfAllIntsAFTERIndex = [];
+  productSoFar = 1;
+  for (var j = intArray.length - 1; j >= 0; j--){
+    // since each index in RESULT already has the product of all integers before, now just store the total product of all otehr integers...
+    productsOfAllIntsExceptAtIndexResult[j] *= productSoFar;
+    productSoFar *= intArray[j];
+  }
+  // [40, 10, 1]
+
+  return productsOfAllIntsExceptAtIndexResult;
+}
+
+// O(n) time and O(n) space.
