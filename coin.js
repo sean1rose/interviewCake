b8dcc1a9d4f7977d8c14191d43c99594fe52eac2
Sean Rose <sean1rose@gmail.com>
2017-02-20 19:00:45 -0800

    list datastructure added from 'Data Structures and Algorithms with Javascript'

--- coin.js
+++ coin.js
@@ -8,39 +8,58 @@ function Change() {
 }
 
 Change.prototype.changePossibilitiesTopDown = function(amountLeft, denominations, currentIndex) {
+    
     currentIndex = currentIndex || 0;
+    console.log('----- amountLeft: ', amountLeft, ' | denominations: ', denominations, ' | currentIndex: ', currentIndex);
 
-    // check our memo and short-circuit if we've already solved this one
+    // check our memo to see if we've seen this set of inputs before, and if so, grab our old answer and return that...
     var memoKey = String([amountLeft, currentIndex]);
+    console.log(`memoKey is ${memoKey}`);
     if (this.memo.hasOwnProperty(memoKey)) {
-        console.log('grabbing memo[' + memoKey + ']');
+        console.log('*answer to that problem (memoKey) - ', memoKey, 'has been stored already -> grabbing memo[' + memoKey + '] - ', this.memo[memoKey]);
         return this.memo[memoKey];
     }
 
     // base cases:
     // we hit the amount spot on. yes!
-    if (amountLeft === 0) return 1;
+    if (amountLeft === 0){
+      console.log('BASE CASE #1: amountLeft === 0 so hit amount spot on!, amountLeft === ', amountLeft);
+      return 1;  
+    } 
 
     // we overshot the amount left (used too many coins)
-    if (amountLeft < 0) return 0;
+    if (amountLeft < 0){
+        console.log('BASE CASE #2: amountLeft is < 0, so we used too many coins, amountLeft === ', amountLeft);
+        return 0;
+    }
 
     // we're out of denominations
-    if (currentIndex === denominations.length) return 0;
+    if (currentIndex === denominations.length){
+        console.log('BASE CASE #3: out of denominations!, currentIndex === denominations.length === ', currentIndex);
+        return 0;
+    }
 
-    console.log('checking ways to make ' + amountLeft + ' with ' + denominations.slice(currentIndex));
+    console.log('NO BASE CASE HIT, so checking ways to make ' + amountLeft + ' with ' + denominations.slice(currentIndex));
 
     // choose a current coin
     var currentCoin = denominations[currentIndex];
+    console.log('currentCoin - ', currentCoin);
 
     // see how many possibilities we can get
     // for each number of times to use currentCoin
     var numPossibilities = 0;
     while (amountLeft >= 0) {
         numPossibilities += this.changePossibilitiesTopDown(amountLeft, denominations, currentIndex + 1);
+        console.log(`adding ${this.changePossibilitiesTopDown(amountLeft, denominations, currentIndex + 1)} to numPossibilities. New numPossibilities is - ${numPossibilities} `);
+        console.log(`subtracting currentCoin (${currentCoin}) from amountLeft (${amountLeft})`);
         amountLeft -= currentCoin;
+        console.log(`new amountLeft after that ^ subtraction is ${amountLeft}`);
     }
 
     // save the answer in our memo so we don't compute it again
     this.memo[memoKey] = numPossibilities;
+    console.log(`saving ${numPossibilities} to this.memo[memoKey] (${this.memo[memoKey]})`);
+    console.log('returning !!! numPossibilities, which  is - ', numPossibilities);
+    console.log('XXXXXXXXXXXXXXXXXXX');
     return numPossibilities;
 };
